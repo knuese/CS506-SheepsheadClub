44797814624ee3372ab9776c5a3ce5276d33c563
Ryan Knuese <knuese@wisc.edu>
2019-03-10 16:14:40 -0500

    Added pug files for all the pages and linked them to the menu

---
+++ routes/pages.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET about page */
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+/* GET players page */
+router.get('/players', (req, res, next) => {
+  res.render('players');
+});
+
+/* GET rules page */
+router.get('/rules', (req, res, next) => {
+  res.render('rules');
+});
+
+/* GET scores page */
+router.get('/scores', (req, res, next) => {
+  res.render('scores');
+});
+
+/* GET tutorial page */
+router.get('/tutorial', (req, res, next) => {
+  res.render('tutorial');
+});
+
+module.exports = router;
