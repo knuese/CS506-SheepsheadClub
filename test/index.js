53772b1801203a25ba362a0bdca25573b6cdc3bc
epwilliams26 <evanpwilliams26@gmail.com>
2019-03-29 10:08:17 -0500

    added unit tests to check for elements on homepage

---
+++ test/index.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const app = require('../app');
+const assert = require('assert');
+
+// Run tests with "npm test"
+describe('Should check for elements on the homepage', () => {
+    it('Has the Scores card', (done) => {
+        request(app).get('/').end((err, res) => {
+            assert(res.text.includes('<h3 class="card-title">Scores</h3>'));
+            done();
+        });
+    });
+
+    it('Has the Cheat Sheet card', (done) => {
+        request(app).get('/').end((err, res) => {
+            assert(res.text.includes('<h3 class="card-title">Cheat Sheet</h3>'));
+            done();
+        });
+    });
+
+    it('Has the Tutorials card', (done) => {
+        request(app).get('/').end((err, res) => {
+            assert(res.text.includes('<h3 class="card-title">Tutorials</h3>'));
+            done();
+        });
+    });
+
+    it('Has the About card', (done) => {
+        request(app).get('/').end((err, res) => {
+            assert(res.text.includes('<h3 class="card-title">About</h3>'));
+            done();
+        });
+    });
+
+    it('Has the Announcements card', (done) => {
+        request(app).get('/').end((err, res) => {
+            assert(res.text.includes('<h3 class="card-title">Announcements</h3>'));
+            done();
+        });
+    });
+});
