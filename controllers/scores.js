997689b62713e4895023b218e3e734b91d78d818
Ryan Knuese <knuese@wisc.edu>
2019-04-07 18:20:29 -0500

    Added date selector for score input

--- controllers/scores.js
+++ controllers/scores.js
@@ -10,14 +10,13 @@ router.get('/scores', (req, res, next) => {
   
 /* GET score entry page */
 router.get('/enter-scores', (req, res, next) => {
-    if (firebase.auth().currentUser) {
+    //if (firebase.auth().currentUser) {
         getPlayers().then((players) => {
-            players.forEach(p => console.log(p.id, p.name));
             res.render('enter-scores', { admin: true, players: players });
         });
-    } else {
-        res.redirect('/login');
-    }
+    // } else {
+    //     res.redirect('/login');
+    // }
 });
 
 router.post('/scores', function(req, res){
@@ -25,15 +24,13 @@ router.post('/scores', function(req, res){
 });
 
 router.post('/enter-scores', function(req, res){
-  res.redirect('enter-scores');
+    res.redirect('enter-scores');
 });
 
 // Loads the players from the database
 async function getPlayers() {
     const snapshot = await firebase.firestore().collection('players').get();
-    let players = [];
-    snapshot.docs.map(doc => players.push(new Player(doc.id, doc.data().name)));
-    return players;
+    return Array.from(snapshot.docs.map(doc => new Player(doc.id, doc.data().name)));
 }
 
 module.exports = router;
