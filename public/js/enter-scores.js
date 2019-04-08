b1bda79c556533e081a884b0fe0ad419502879f7
Ryan Knuese <knuese@wisc.edu>
2019-04-07 21:13:51 -0500

    Can add scores to the database

--- public/js/enter-scores.js
+++ public/js/enter-scores.js
@@ -1,17 +1,66 @@
 const playerSelect = $('select#player');
 const semesterInput = $('input#semester');
 const datePicker = $('input#date-picker');
-const quickAddName = $('input#name');
+const scoreInput = $('input#score');
+const saveScoreButton = $('button#save-score');
+const clearButton = $('button#clear');
+const saveErrLabel = $('label#save-error');
 
-$(document).ready(() => {
-    datePicker.val(todayFormatted());
-    semesterInput.prop('disabled', true);
-    semesterInput.val(calculateSemester(datePicker.val()));
-});
+const quickAddName = $('input#name');
+const savePlayerButton = $('button#save-player');
+
+// Initialize the page
+playerSelect.val('');
+datePicker.val(todayFormatted());
+semesterInput.prop('disabled', true);
+semesterInput.val(calculateSemester(datePicker.val()));
 
 // When a different date is selected, recalculate the semester
 $(datePicker).change(() => {
     semesterInput.val(calculateSemester(datePicker.val()));
+});
+
+// Handler for the Save button to save a score
+$(saveScoreButton).click(() => {
+    let playerId = playerSelect.val();
+    let semester = semesterInput.val();
+    let date = datePicker.val();
+    let score = scoreInput.val();
+
+    if (!playerId || !score) {
+        $(saveErrLabel).css('display', 'inline-block');
+        // Hide label after 1 second
+        setTimeout(() => saveErrLabel.css('display', 'none'), 1000);
+    } else {
+        // Save score
+        $.post('/enter-scores/save-score', {
+            playerId: playerId,
+            semester: semester,
+            date: date,
+            score: score
+        })
+        .then(location.reload())
+        .catch((err) => console.error(err));
+    }
+});
+
+// Handler for when the Clear button is clicked
+$(clearButton).click(() => {
+    playerSelect.val('');
+    scoreInput.val('');
+});
+
+// Handler for when the Save button is clicked for adding a player quickly
+$(savePlayerButton).click(() => {
+    let name = quickAddName.val();
+
+    if (name) {
+        $.post('/enter-scores/add-player', {name: name})
+            .then(location.reload())
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 });
 
 // Format today's date so that the input element will accept it
@@ -36,17 +85,3 @@ function calculateSemester(date) {
 
     return `${season} '${year}`;
 }
-
-// Handler for when the save button is clicked for adding a player quickly
-$('#save-player').click(() => {
-    let name = quickAddName.val();
-
-    if (name) {
-        $.post('/enter-scores/add-player', {name: name})
-            .then(location.reload())
-            .catch((err) => {
-                console.error(err);
-            }
-        );
-    }
-});
