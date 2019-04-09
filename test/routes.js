66c0bc230dd29c4c7cb30461cd2b2a35ba31784c
Ryan Knuese <knuese@wisc.edu>
2019-04-09 14:04:10 -0500

    POST unit tests

--- test/routes.js
+++ test/routes.js
@@ -7,6 +7,10 @@ describe('Should get all pages of the website', () => {
         request(app).get('/').expect(200, done);
     });
 
+    it('GET /login', (done) => {
+        request(app).get('/login').expect(200, done);
+    });
+
     it('GET /about', (done) => {
         request(app).get('/about').expect(200, done);
     });
@@ -29,6 +33,41 @@ describe('Should get all pages of the website', () => {
 
     it('GET /help', (done) => {
         request(app).get('/help').expect(200, done);
+    });
+});
+
+// Code 302 = redirect
+describe('Should redirect when making POST request for pages', () => {
+    it('POST /', (done) => {
+        request(app).post('/').expect(302, done);
+    });
+
+    it('POST /about', (done) => {
+        request(app).post('/about').expect(302, done);
+    });
+
+    it('POST /players', (done) => {
+        request(app).post('/players').expect(302, done);
+    });
+
+    it('POST /rules', (done) => {
+        request(app).post('/rules').expect(302, done);
+    });
+
+    it('POST /enter-scores', (done) => {
+        request(app).post('/enter-scores').expect(302, done);
+    });
+
+    it('POST /scores', (done) => {
+        request(app).post('/scores').expect(302, done);
+    });
+
+    it('POST /help', (done) => {
+        request(app).post('/help').expect(302, done);
+    });
+    
+    it('POST /logout', (done) => {
+        request(app).post('/logout').expect(302, done);
     });
 });
 
