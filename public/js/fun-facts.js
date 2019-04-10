e4799d6ba315177e8f70741ba4734394ef26763d
Ryan Knuese <knuese@wisc.edu>
2019-04-10 15:56:19 -0500

    Filled in history of sheepshead page

--- public/js/fun-facts.js
+++ public/js/fun-facts.js
@@ -1,20 +1,11 @@
+$('button.fun').on('click', (event) => {
+    $('button.fun').removeClass('selected');
+    $(event.target).addClass('selected');
+    displayForSelection(event.target.id);
+});
+
 $(document).ready(() => {
-    $('button.fun').on('click', (event) => {
-        $('button.fun').removeClass('selected');
-        $(event.target).addClass('selected');
-        displayForSelection(event.target.id);
-    });
-    /*
-    const roleDropdown = $('select#role');
-
-    roleDropdown.change(() => {
-        console.log(`/rules/${roleDropdown.val()}.html`);
-        $('#cheetsheet-text').load(`rules/${roleDropdown.val()}.html`);
-    });*/
-
-    // Start with general cheetsheet selected
-    $('button#cheatsheet').trigger('click');
-    //roleDropdown.trigger('change');
+    $('button#vocab').trigger('click');
 });
 
 function displayForSelection(id) {
