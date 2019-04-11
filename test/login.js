e3efad56406bc9cd272337c2bc916dcd3d28de44
NafisFaisalKhan <nafisfaisalkhan@gmail.com>
2019-04-10 19:48:33 -0500

    logut test

--- test/login.js
+++ test/login.js
@@ -2,12 +2,21 @@ const request = require('supertest');
 const app = require('../app');
 const assert = require('assert');
 
-describe('Should Login', () => {
+describe('Should Login and Logout', () => {
     it('Logs in', (done) => {
         request(app).post('/login')
         .send({email:"sheepshead.test@gmail.com", password:"cs5062019!"})
         .expect(200)
         .end(done);
     });
+
+    it('Logs out', (done) => {
+        request(app).post('/logout')
+        .expect(302)
+        .end(done);
+    });
+    
+
+
 
 });
