7c794825af6a8fc72d0e049787bc2946dc849c24
NafisFaisalKhan <nafisfaisalkhan@gmail.com>
2019-04-10 20:02:29 -0500

    login tests and logout test added

--- test/login.js
+++ test/login.js
@@ -10,13 +10,33 @@ describe('Should Login and Logout', () => {
         .end(done);
     });
 
+    it('Has the Enter Score button', (done) => {
+        request(app).get('/layout').end((err, res) => {
+            assert(res.text.includes(`<button class="btn-block side-button" type="Submit">Enter Scores</button>`)); 
+            done();
+        });
+    });
+
+    it('Has the Manage Player button', (done) => {
+        request(app).get('/layout').end((err, res) => {
+            assert(res.text.includes(`<button class="btn-block side-button" type="Submit">Manage Players</button>`)); 
+            done();
+        });
+    });
+    
+    it('Has the logout button', (done) => {
+        request(app).get('/layout').end((err, res) => {
+            assert(res.text.includes(`<button class="btn-block side-button" type="Submit">Logout</button>`)); 
+            done();
+        });
+    });
+
+
+
     it('Logs out', (done) => {
         request(app).post('/logout')
         .expect(302)
         .end(done);
     });
     
-
-
-
 });
