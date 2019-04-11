14439e3f51bd05bc9dc619ef1f9d0ff496c632fe
Xiangjun Ma <xj.m@hotmail.com>
2019-04-11 18:39:28 -0500

    change edit to delete, and working on route/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,12 @@ let express = require('express');
 let router = express.Router();
 let firebase = require("firebase");
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { admin: firebase.auth().currentUser != null });
-});
+exports.post = (req, res, next) => {
+  res.redirect('/');
+};
 
-router.post('/', function(req, res){
-    res.redirect('/');
-});
-
-module.exports = router;
+exports.getHomePage = (req, res, next) => {
+  res.render('index', {
+    admin: firebase.auth().currentUser != null
+  });
+};
