b1e48aeb025ce5f1b02b94014ed01f5e65e2c084
epwilliams26 <evanpwilliams26@gmail.com>
2019-04-12 00:27:57 -0500

    Added Admin homepage functionality.  Admin should be able to add and delete announcements.  Includes database functionality for storing and removing posts.

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,64 @@ let express = require('express');
 let router = express.Router();
 let firebase = require("firebase");
 
+class Announcement {
+    constructor(timestamp, posterName, date, content, id) {
+        this.timestamp = timestamp;
+        this.posterName = posterName;
+        this.date = date;
+        this.content = content;
+        this.id = id;
+    }
+}
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { admin: firebase.auth().currentUser != null });
+  getAnnouncements().then((announcements) => {
+      res.render('index', { admin: firebase.auth().currentUser != null, announcements: announcements });
+  });
 });
 
 router.post('/', function(req, res){
     res.redirect('/');
 });
+
+router.post('/submit_post', function(req, res){
+
+    var js_date = new Date();
+    var date = js_date.toDateString();
+    var timestamp = firebase.firestore.Timestamp.now();
+
+    firebase.firestore().collection('announcements').add({
+        timestamp: timestamp.seconds,
+        posterName: req.body.posterName,
+        date: date,
+        content: req.body.content
+    }).then(() => {
+        res.send("success");
+    }).catch((err) => {
+        res.status(500);
+        res.statusMessage = err;
+        res.send();
+    });
+});
+
+router.post('/delete_post', function(req, res){
+    firebase.firestore().collection('announcements').doc(req.body.id).delete().then(() => {
+        res.send("success");
+    }).catch((err) => {
+        res.status(500);
+        res.statusMessage = err;
+        res.send();
+    });
+
+});
+
+async function getAnnouncements() {
+    const snapshot = await firebase.firestore().collection('announcements').orderBy('timestamp').get();
+    let announcements = Array.from(snapshot.docs.map(doc =>
+        new Announcement(doc.data().timestamp, doc.data().posterName, doc.data().date, doc.data().content, doc.id)));
+    announcements = announcements.reverse();
+    return announcements;
+}
 
 module.exports = router;
---
+++ public/js/index.js
@@ -0,0 +1,41 @@
+const postButton = $('button#postButton');
+const postContent = $('textarea#postContent');
+const poster = $('input#poster');
+
+//add event listener to each delete button for every post dynamically
+Array.from(document.querySelectorAll('.deleteButton')).forEach(function(button) {
+    $('#'+button.id).on('click', (event) => {
+        $.ajax({
+          type: 'POST',
+          url: '/delete_post',
+          data: {
+              id: button.id
+          },
+          success: function(data){
+            if(data == 'success'){
+                location.reload();
+            }
+          }
+        });
+    });
+});
+
+postButton.on('click', (event) => {
+
+    let posterName = poster.val();
+    let content = postContent.val();
+
+    $.ajax({
+      type: 'POST',
+      url: '/submit_post',
+      data: {
+          posterName: posterName,
+          content: content
+      },
+      success: function(data){
+        if(data == 'success'){
+            location.reload();
+        }
+      }
+    });
+});
