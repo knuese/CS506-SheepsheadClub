bb2814f9a442fd502ca56b9aafe4ef54e2ae5646
Blake Baranowski <baranowski2@wisc.edu>
2019-04-19 02:02:23 -0500

    Added quite a bit of functionality to the Should I Pick for randomized interactive questions

--- public/js/help.js
+++ public/js/help.js
@@ -1,10 +1,16 @@
 const roleDropdown = $('select#strategies-role');
 const playDropdown = $('select#play-role');
 const cardDropdown = $('select#card-dropdown');
+const simulatorDropdown = $('select#picking-simulator');
 const pickDropdown = $('select#picking-examples');
 let dupErrorLabel = $('label#dup-error');
 let correctPickLabel = $('label#correct-pick');
+let maybePickLabel = $('label#maybe-pick');
 let incorrectPickLabel = $('label#incorrect-pick');
+
+let random_hand = [];
+
+let simulatorFlag = false;
 
 // Array of cards that have been selected
 let hand = [];
@@ -12,8 +18,8 @@ let hand = [];
 $(document).ready(() => {
     // Start with general strategy selected
     $('button#strategy').trigger('click');
-    $('button#pickup').trigger('click');
-    $('button#pass').trigger('click');
+    $('button#pickuphard').trigger('click');
+    $('button#passhard').trigger('click');
 
     roleDropdown.trigger('change');
     playDropdown.trigger('change');
@@ -34,8 +40,24 @@ playDropdown.change(() => {
     $('#play-text').load(`what-to-play/${playDropdown.val()}.html`);
 });
 
+simulatorDropdown.change(() => {
+    if(simulatorDropdown.val() == "hard_coded") {
+        simulatorFlag = false;
+        $('.hardcode').css('display', 'block');
+        $('.hardcode#pick-text').load(`should-I-pick/${pickDropdown.val()}.html`);
+        $('.random').css('display', 'none');
+    }
+    else {
+        simulatorFlag = true;
+        $('.random').css('display', 'block');
+        $('.hardcode').css('display', 'none');
+        randomHand();
+
+    }
+});
+
 pickDropdown.change(() => {
-    $('#pick-text').load(`should-I-pick/${pickDropdown.val()}.html`);
+    $('.hardcode#pick-text').load(`should-I-pick/${pickDropdown.val()}.html`);
 });
 
 // Hide the error when a different card is selected so as not to be confusing
@@ -86,9 +108,8 @@ $('button#add-button').on('click', () => {
     }
 });
 
-$('button#pickup').on('click', () => {
-    $('label#correct-pick').css('display', 'none');
-    $('label#incorrect-pick').css('display', 'none');
+$('button#pickuphard').on('click', () => {
+    clearMessage();
 
     if(pickDropdown.val() == 'example1' || pickDropdown.val() == 'example3') {
         $('label#correct-pick').css('display', 'block');
@@ -105,9 +126,8 @@ $('button#pickup').on('click', () => {
     }
 });
 
-$('button#pass').on('click', () => {
-    $('label#correct-pick').css('display', 'none');
-    $('label#incorrect-pick').css('display', 'none');
+$('button#passhard').on('click', () => {
+    clearMessage();
 
     if(pickDropdown.val() == 'example1' || pickDropdown.val() == 'example3') {
         $('label#incorrect-pick').css('display', 'block');
@@ -122,6 +142,59 @@ $('button#pass').on('click', () => {
         // Hide label after 10 second
         setTimeout(() => correctPickLabel.css('display', 'none'), 10000);
     }
+});
+
+$('button#pickuprandom').on('click', () => {
+    clearMessage();
+
+    check_hand = checkHand();
+    if(check_hand == 1){
+        $('label#correct-pick').css('display', 'block');
+
+        // Hide label after 10 second        
+        setTimeout(() => correctPickLabel.css('display', 'none'), 10000);
+    }
+    else if(check_hand == 2){
+        $('label#maybe-pick').css('display', 'block');
+        
+        // Hide label after 10 second        
+        setTimeout(() => maybePickLabel.css('display', 'none'), 10000);
+    }
+    else {
+        $('label#incorrect-pick').css('display', 'block');
+
+        // Hide label after 10 second
+        setTimeout(() => incorrectPickLabel.css('display', 'none'), 10000);
+    }
+});
+
+$('button#passrandom').on('click', () => {
+    clearMessage();
+
+    check_hand = checkHand();
+    if(check_hand == 0){
+        $('label#correct-pick').css('display', 'block');
+
+        // Hide label after 10 second        
+        setTimeout(() => correctPickLabel.css('display', 'none'), 10000);
+    }
+    else if(check_hand == 2){
+        $('label#maybe-pick').css('display', 'block');
+        
+        // Hide label after 10 second        
+        setTimeout(() => maybePickLabel.css('display', 'none'), 10000);
+    }
+    else {
+        $('label#incorrect-pick').css('display', 'block');
+
+        // Hide label after 10 second
+        setTimeout(() => incorrectPickLabel.css('display', 'none'), 10000);
+    }
+});
+
+$('button#newdeal').on('click', () => {
+    clearMessage();
+    randomHand();
 });
 
 // Remove a card from both the hand array and the UI
@@ -136,8 +209,89 @@ function removeCard(event) {
     toRemove.remove();
 };
 
+function clearMessage(){
+    $('label#correct-pick').css('display', 'none');
+    $('label#maybe-pick').css('display', 'none');
+    $('label#incorrect-pick').css('display', 'none');
+}
+
+function checkHand() {
+    let trump_count = 0;
+    let queen_count = 0;
+    let jack_count = 0;
+    let total_points = 0;
+    for(let i = 0; i < random_hand.length; i++) {
+        if(random_hand[i].code[0] == "Q"){
+            queen_count++;
+        }
+        if(random_hand[i].code[0] == "J"){
+            jack_count++;
+        }
+        if(random_hand[i].isTrump == true){
+            trump_count++;
+        }
+        total_points += random_hand[i].pointValue;
+    }
+
+    if(queen_count >= 3){
+        return 1;
+    }
+    else if(queen_count >= 2 && trump_count >= 3){
+        return 1;
+    }
+    else if(trump_count >= 4){
+        return 1;
+    }
+    else if(queen_count >= 2 && total_points >= 35){
+        return 2;
+    }
+    else if(trump_count >= 3 && total_points >=30){
+        return 2;
+    }
+    else {
+        return 0;
+    }
+};
+
+// Retreives a random hand for the Should I Pick?
+function randomHand() {
+    
+    let cardsRow = $('div#pick-text-random');
+    $('.random').css('display', 'flex');
+
+    
+    $(".displayed").remove();
+
+    random_hand = [];
+    let random_num_list = [];
+    
+    for(let i = 0; i < 6; i++) {
+        let randomNum = Math.floor(Math.random() * 32);
+        if (random_num_list.indexOf(randomNum) == -1) {
+            random_hand.push(cards[randomNum]);
+            random_num_list.push(randomNum);
+        }
+        else {
+            i--;
+        }
+    }
+    
+
+
+    for(let i = 0; i < random_hand.length; i++) {
+        cardsRow.append(`<div class='card my-card displayed' id='${random_hand[i].code}'>
+                            <img src='/images/cards/${random_hand[i].code}.png' class='card'/>
+                            <p><b>${random_hand[i].name}</b><br/>
+                            Point value: ${random_hand[i].pointValue}<br/>
+                            Is trump? ${random_hand[i].isTrump ? 'Yes' : 'No'}</p>
+                        </div>`);
+    }
+
+
+};
+
 // Display the correct help information
 function displayForSelection(id) {
     $('div.help-content').css('display', 'none');
     $(`div#${id}`).css('display', 'block');
-}
+};
